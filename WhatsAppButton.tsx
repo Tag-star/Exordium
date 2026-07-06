523dc2b0ce5e162d52b467e74cf260f43a2acaef
Tag-star <57000808+Tag-star@users.noreply.github.com>
2026-07-06 10:34:34 +0300

    Update WhatsAppButton.tsx

--- WhatsAppButton.tsx
+++ WhatsAppButton.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function WhatsAppButton() {
   return (
     <Link
-      href={`https://wa.me/${+25473449475}`}
+      href={`https://wa.me/${25473449475}`}
       target="_blank"
       rel="noopener noreferrer"
       className="fixed bottom-6 right-6 z-50 bg-[#25D366] text-white w-14 h-14 rounded-full flex items-center justify-center shadow-lg hover:scale-105 transition-transform"
